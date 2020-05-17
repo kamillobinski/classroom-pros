41facb42dd2eba2f502de69205eb7b4f19796f7b
Kamil <kamilobinski@gmail.com>
2020-05-17 04:14:59 +0200

    Deploy the sign-in function

---
+++ src/main/java/com/example/demo/service/SignInService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.UserRepository;
+import com.example.demo.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SignInService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Called when user has submitted sign-in attempt
+    // Object that will be returned will contain data or will remain empty
+    public User CheckUserCredencials(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
