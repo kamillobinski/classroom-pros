56bcd504339315a3f811ed8042ea1b7fd4a165b4
Kamil <kamilobinski@gmail.com>
2020-05-17 18:26:56 +0200

    Show controller messages on sign-in page

--- src/main/java/com/example/demo/controller/SignInController.java
+++ src/main/java/com/example/demo/controller/SignInController.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.User;
 import com.example.demo.service.SignInService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,7 +16,7 @@ public class SignInController {
     private SignInService signInService;
 
     @RequestMapping(value = "/sign-in-action", method = RequestMethod.POST)
-    public String singInToAccount(@RequestParam("sign-in-email-input") String email, @RequestParam("sign-in-password-input") String password) {
+    public String singInToAccount(Model model, @RequestParam("sign-in-email-input") String email, @RequestParam("sign-in-password-input") String password) {
         // Gets registered user with specific email from database
         // If user not in database it will remain as an empty object
         User existingUser = signInService.CheckUserCredencials(email);
@@ -26,6 +27,11 @@ public class SignInController {
         if (existingUser != null && existingUser.getPassword().equals(password)) {
             return "homepage";
         } else {
+            if(email == "" && password == "") {
+                model.addAttribute("message", "No data entered.");
+            } else {
+                model.addAttribute("message", "Incorrect email or password.");
+            }
             return "sign-in";
         }
     }
