d602d3684e52b757ed4552d3339cf4854d5c086c
Damian Bregier <damian.bregier@gmail.com>
2020-05-21 07:33:25 +0200

    Removing group object

--- src/main/java/com/example/demo/dao/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.entity.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends JpaRepository<Group, Integer> {
-}
