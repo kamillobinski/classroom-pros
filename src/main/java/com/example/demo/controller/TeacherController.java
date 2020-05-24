ae649c934d37fc2efd6c2da6e63171731d19850d
nakime <alicja.oraczewska@onet.pl>
2020-05-24 19:10:15 +0200

    Teacher controller

---
+++ src/main/java/com/example/demo/controller/TeacherController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Teacher;
+import com.example.demo.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TeacherController {
+
+    @Autowired
+    private TeacherService teacherService;
+
+    @RequestMapping(value = "/add-teacher-action", method = RequestMethod.POST)
+    public String addNewTeacher(Model model, @RequestParam("add-teacher-firstname-input") String firstName, @RequestParam("add-teacher-lastname-input") String lastName, @RequestParam("add-teacher-title-input") String title) {
+
+        // Check if input values are not empty
+        if (firstName != "" && lastName != "" && title != "") {
+
+            // Creates new teacher based on the entered data in the form
+            Teacher newTeacher = new Teacher();
+            newTeacher.setFirst_name(firstName);
+            newTeacher.setLast_name(lastName);
+            newTeacher.setTitle(title);
+
+            teacherService.addNewTeacher(newTeacher);
+        }else {
+            if(firstName == "" || lastName == "" || title == "") {
+                model.addAttribute("message", "Could not add teacher without data.");
+            }
+        }
+        return "upload";
+    }
+}
