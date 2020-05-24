8c68f9dd6dcf3bac9c5a45dea735264393018df9
dawid <davido123@o2.pl>
2020-05-24 13:35:42 +0200

    Load and view of plan on the main site

--- src/main/java/com/example/demo/controller/PlanController.java
+++ src/main/java/com/example/demo/controller/PlanController.java
@@ -1,16 +1,19 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.Plan;
+import com.example.demo.dao.RoomRepository;
+import com.example.demo.dao.SubjectRepository;
+import com.example.demo.dao.TeacherRepository;
+import com.example.demo.entity.*;
+import com.example.demo.service.HourService;
 import com.example.demo.service.LessonService;
 import com.example.demo.service.PlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Controller
 public class PlanController {
@@ -21,6 +24,18 @@ public class PlanController {
     @Autowired
     private LessonService lessonService;
 
+    @Autowired
+    private HourService hourService;
+
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @Autowired
+    private RoomRepository roomRepository;
+
     // Change plan name
     @RequestMapping("/change-current-plan-name")
     public String changePlanName(Model model, @RequestParam("current-plan-id") int id, @RequestParam("changed-plan-name") String name) {
@@ -30,6 +45,56 @@ public class PlanController {
         planService.savePlan(existingPlan);
 
         return "redirect:/test-home-" + id;
+    }
+
+    @RequestMapping("/homepage-{requestedPlanId}")
+    public String selectPlan(Model model,@PathVariable int requestedPlanId) {
+        List<Plan> plans = planService.getAllPlans();
+        model.addAttribute("plans", plans);
+
+        List<Lesson> mondayLessons = lessonService.getLessonsForSpecificDayAndPlan("Monday", requestedPlanId);
+        model.addAttribute("mondayData", mondayLessons);
+
+        List<Lesson> tuesdayLessons = lessonService.getLessonsForSpecificDayAndPlan("Tuesday", requestedPlanId);
+        model.addAttribute("tuesdayData", tuesdayLessons);
+
+        List<Lesson> wednesdayLessons = lessonService.getLessonsForSpecificDayAndPlan("Wednesday", requestedPlanId);
+        model.addAttribute("wednesdayData", wednesdayLessons);
+
+        List<Lesson> thursdayLessons = lessonService.getLessonsForSpecificDayAndPlan("Thursday", requestedPlanId);
+        model.addAttribute("thursdayData", thursdayLessons);
+
+        List<Lesson> fridayLessons = lessonService.getLessonsForSpecificDayAndPlan("Friday", requestedPlanId);
+        model.addAttribute("fridayData", fridayLessons);
+
+        List<Plan> allPlans = planService.getAllPlans();
+        model.addAttribute("allPlans", allPlans);
+
+        Plan currentPlan = planService.getPlanById(requestedPlanId);
+        model.addAttribute("currentPlan", currentPlan);
+
+        List<Hour> allHours = hourService.getHours();
+        model.addAttribute("hourData", allHours);
+
+        // Used in the lesson edit form
+        List<Subject> allSubjects = subjectRepository.findAll();
+        model.addAttribute("allSubjects", allSubjects);
+
+        List<Teacher> allTeachers = teacherRepository.findAll();
+        model.addAttribute("allTeachers", allTeachers);
+
+        List<Room> allRooms = roomRepository.findAll();
+        model.addAttribute("allRooms", allRooms);
+
+        return "homepage";
+    }
+
+    @RequestMapping("/get-first-plan")
+    public String getFirstPlan(Model model){
+        int firstPlanId = planService.getAllPlans().get(0).getId();
+        System.out.println("clicked");
+        return "redirect:/homepage-"+firstPlanId;
+
     }
 
     @Transactional
