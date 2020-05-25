ac0d0f6e20fe80fe342d602f97a88eca3ea47d0e
Kamil <kamilobinski@gmail.com>
2020-05-26 01:55:53 +0200

    Update route

--- src/main/java/com/example/demo/controller/RouteController.java
+++ src/main/java/com/example/demo/controller/RouteController.java
@@ -1,9 +1,18 @@
 package com.example.demo.controller;
+import com.example.demo.entity.Plan;
+import com.example.demo.service.PlanService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class RouteController {
+
+    @Autowired
+    private PlanService planService;
 
     // Sign in page
     @RequestMapping("/sign-in")
@@ -27,5 +36,13 @@ public class RouteController {
     @RequestMapping("/upload")
     public String getAddItem() {
         return "upload";
+    }
+
+    // All plans
+    @RequestMapping("/plans")
+    public String getPlans(Model model) {
+        List<Plan> allPlans = planService.getAllPlans();
+        model.addAttribute("allPlans", allPlans);
+        return "plans";
     }
 }
