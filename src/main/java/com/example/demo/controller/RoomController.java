872fd864d63d29c37607f6376cc7ab2e85108917
nakime <alicja.oraczewska@onet.pl>
2020-05-25 17:11:51 +0200

    Add Room controller and service

---
+++ src/main/java/com/example/demo/controller/RoomController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Room;
+import com.example.demo.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class RoomController {
+
+    @Autowired
+    private RoomService roomService;
+
+    @RequestMapping(value = "/add-room-action", method = RequestMethod.POST)
+    public String addNewRoom(Model model, @RequestParam("add-room-number-input") String number) {
+
+        // Check if input values are not empty
+        if (number != "") {
+
+            // Creates new room based on the entered data in the form
+            Room newRoom = new Room();
+            newRoom.setNumber(number);
+
+            roomService.addNewRoom(newRoom);
+        }else {
+            if(number == "") {
+                model.addAttribute("message", "Could not add room without data.");
+            }
+        }
+        return "upload";
+    }
+}
---
+++ src/main/java/com/example/demo/service/RoomService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.RoomRepository;
+import com.example.demo.entity.Room;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomService {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    public Room addNewRoom(Room room){
+        return roomRepository.save(room);
+    }
+}
