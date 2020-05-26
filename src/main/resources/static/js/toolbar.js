550aa980fd2672176a72f0c61a6b4293e1ba0b00
Kamil <kamilobinski@gmail.com>
2020-05-27 00:22:39 +0200

    Reveal lesson editor when lesson selected

---
+++ src/main/resources/static/js/toolbar.js
@@ -0,0 +1,25 @@
+function showLessonEditor() {
+    const accordions = document.getElementsByClassName("accordion");
+    const lessonPanel = document.getElementById('accordion--lesson--panel');
+    const path = window.location.pathname.split("-");
+    let i;
+
+    // Default accordion behavior
+    for (i = 0; i < accordions.length; i++) {
+        accordions[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            const panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+
+    // Opening lesson editor when path is '/request-lesson-update'
+    if (path[0] == "/request" && path[1] == "lesson" && path[2] == "update") {
+        accordions[1].classList.toggle("active");
+        lessonPanel.style.maxHeight = lessonPanel.scrollHeight + "px";
+    }
+}
