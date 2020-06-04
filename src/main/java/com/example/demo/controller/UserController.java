dff3897d1735e2428336f0cb31cf61761ac85f59
Damian Bregier <damian.bregier@gmail.com>
2020-06-04 17:13:00 +0200

    Add user manager

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Role;
 import com.example.demo.entity.User;
+import com.example.demo.repository.RoleRepository;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.List;
 
 @Controller
@@ -15,6 +19,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
     @GetMapping("/users-view")
     public  String findAllUsers(Model model){
         List <User> allUsers = userService.getUsers();
@@ -22,5 +33,31 @@ public class UserController {
         return "users-view";
     }
 
+    @GetMapping("/user-manager")
+    public String getUserManager(){
+        return "user-manager";
+    }
 
+    @RequestMapping(value = "/add-user-action", method = RequestMethod.POST)
+    public String addUser(Model model, @RequestParam("add-user-email-input") String email, @RequestParam("add-user-password-input")String password){
+
+        if(email!="" && password!=""){
+
+            User newUser = new User();
+            newUser.setEmail(email);
+
+            String hashedPassword;
+            newUser.setPassword(bCryptPasswordEncoder.encode(password));
+
+            Role userRole = roleRepository.findByRole("USER");
+            newUser.setRoles(new HashSet<>(List.of(userRole)));
+
+            userService.saveUser(newUser);
+        }else{
+            if(email== "" || password == ""){
+                model.addAttribute("message", "Could not add user without data");
+            }
+        }
+        return "admin-panel";
+    }
 }
