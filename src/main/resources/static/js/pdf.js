a731aaff4fe96e50827193836f57805384f7e0f1
kamillobinski <kamil.lobinski@gmail.com>
2020-11-25 17:04:53 +0100

    ADD: pdf document generator

---
+++ src/main/resources/static/js/pdf.js
@@ -0,0 +1,11 @@
+function generatePDF() {
+    const name = document.getElementById("plan-name");
+    html2canvas($("#pdf-content"), {
+        onrendered: function(canvas) {
+            var pdf = new jsPDF('landscape');
+            var imgData = canvas.toDataURL('image/png');
+            pdf.addImage(imgData, 'PDF', 20, 40, canvas.width*0.2, canvas.height*0.2);
+            pdf.save(name.textContent);
+        }
+    });
+}
